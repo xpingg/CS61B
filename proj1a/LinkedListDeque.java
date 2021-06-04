68cb0bb5bab62426855e5c6362f64ae56fee0dc5
you <you@qq.com>
2021-06-04 21:53:47 +0800

    finish linkedListDeque

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,122 @@
+public class LinkedListDeque<T>
+{
+    private class typeNode
+    {
+        T item;
+        typeNode prev;
+        typeNode next;
+
+        public typeNode(T _item, typeNode _prev, typeNode _next) {
+            item = _item;
+            prev = _prev;
+            next = _next;
+        }
+
+    }
+
+    private typeNode sentinel;
+
+    private int size = 0;
+
+    public LinkedListDeque()
+    {
+        sentinel = new typeNode(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    public void addFirst(T item)
+    {
+        typeNode addedNode = new typeNode(item, null, null);
+        addedNode.prev = sentinel;
+        addedNode.next = sentinel.next;
+        sentinel.next = addedNode;
+        size++;
+    }
+
+    public void addLast(T item)
+    {
+        typeNode addedNode = new typeNode(item, null, null);
+        addedNode.prev = sentinel.prev;
+        addedNode.next = sentinel;
+        sentinel.prev = addedNode;
+        size++;
+    }
+
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    public int size()
+    {
+        return size;
+    }
+
+    public void printDeque()
+    {
+        typeNode p = sentinel;
+        while(p.next!=sentinel)
+        {
+            p = p.next;
+            System.out.print(p.item);
+            System.out.print(" ");
+        }
+    }
+
+    public T removeFirst()
+    {
+        T returnItem = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return returnItem;
+    }
+
+    public T removeLast()
+    {
+        T returnItem = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return returnItem;
+    }
+
+    public T get(int index)
+    {
+        typeNode p = sentinel.next;
+        T returnItem = p.item;
+        int currentIndex = 0;
+        while(p.next != sentinel && currentIndex < index)
+        {
+            p = p.next;
+            returnItem = p.item;
+        }
+
+        return returnItem;
+
+    }
+
+    private T getRecursive(int index,typeNode p)
+    {
+        if(index == 0 || p.next == sentinel)
+        {
+            return p.next.item;
+        }
+        else
+        {
+            return getRecursive(index - 1,p.next);
+        }
+
+    }
+
+    public T getRecursive(int index)
+    {
+        return getRecursive(index,sentinel);
+    }
+
+
+
+}
+
+
